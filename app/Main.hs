f3ef4124a166e5c8c2e392eb3180e4f2fcaace11
Franklin Chen <franklinchen@franklinchen.com>
2015-10-13 13:53:14 -0400

    Final command-line app.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,23 @@
 module Main where
 
 import Control.Arrow ((>>>))
+import Data.Function ((&))
+import Text.Printf (printf)
 
-import DoubledWords (WordInfo(..), doubledWords)
+import DoubledWords (OurWord(..), WordInfo(..), doubledWords)
 
 -- | Read from standard input, analyze for doubled words, print a report.
+--
+-- Note an interesting feature: because of laziness, this program will
+-- immediately report a run of duplicates as soon as it sees the end of one.
 main :: IO ()
 main = getContents
        >>= (doubledWords >>> printReport)
 
 printReport :: [WordInfo] -> IO ()
-printReport = undefined
+printReport = mapM_ printWordInfo
+
+-- | Print report to standard output.
+printWordInfo :: WordInfo -> IO ()
+printWordInfo info =
+  printf "%d %s\n" (info & _line) (info & _word & _text)
